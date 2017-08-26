8ef9f2e613af114f2fe944c7865aa3f1ea2ea30a
Jessele V Del Mundo <skyeunknown084@gmail.com>
2017-08-26 19:11:28 +0800

    Routes Connections
    Create routes to links in main page

--- index.php
+++ index.php
@@ -18,12 +18,17 @@
     <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
     <!-- Plugin CSS -->
+    <!-- <link rel="stylesheet" type="text/css" href="css/cloud-admin.css" > -->
     <link href="vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
-
+    <!-- <link rel="stylesheet" type="text/css" href="js/fullcalendar/fullcalendar.min.css" /> -->
     <!-- Custom styles for this template -->
     <link href="css/sb-admin.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
 
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4/js/bootstrap.min.js"></script> -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
   </head>
 
   <body class="fixed-nav bg-dark" id="page-top">
@@ -63,6 +68,33 @@
       </div>
     </div>
 
+     <div class="modal fade" id="aboutUs" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">About Us</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body" >
+            <div class="panel" style="border-color: blue; border: 1px;">
+              Web-based Employee Information System <br/>
+            <label class="text-center"><b>Members:</b></label> <br/>
+            <label class="text-center">J1</label> <br/>
+            <label class="text-center">J2</label> <br/>
+            <label class="text-center">J3</label> <br/>
+            </div>
+            
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-primary" data-dismiss="modal">OK</button>
+            <!-- <a class="btn btn-primary" href="login.html">OK</a> -->
+          </div>
+        </div>
+      </div>
+    </div>
+
     <!-- Bootstrap core JavaScript -->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/popper/popper.min.js"></script>
@@ -77,6 +109,26 @@
     <!-- Custom scripts for this template -->
     <script src="js/sb-admin.min.js"></script>
     <script type="text/javascript" src="js/functions/navigate.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+
+<!-- Latest compiled and minified Bootstrap JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+    <!-- <script type="text/javascript" src="js/fullcalendar/fullcalendar.min.js"></script> -->
+    <script>
+// Initialize tooltip component
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+// Initialize popover component
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
+function toggler(divId) {
+    $("#" + divId).toggle();
+}
+</script>
   </body>
 
 </html>
